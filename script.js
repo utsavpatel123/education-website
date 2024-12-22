198a3263ab3e04dd9494cc481216608bfe3d8ab7
utsav patel <utsavp377@gmail.com>
2024-12-22 22:49:35 +0530

    Add

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function opentab(){
+    var open = document.querySelector('nav .navigation ul');
+     open.style.right = "0px";
+}
+
+function closetab(){
+    var close = document.querySelector('nav .navigation ul');
+      close.style.right = "-220px";
+}
